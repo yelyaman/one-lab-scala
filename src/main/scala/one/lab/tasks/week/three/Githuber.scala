ff25c686fdaf4b6573e245bc0d3cfbb7f47b2076
Yelyaman <yelaman.ui@gmail.com>
2020-07-09 11:52:16 +0600

    Task Githuber completed

--- src/main/scala/one/lab/tasks/week/three/Githuber.scala
+++ src/main/scala/one/lab/tasks/week/three/Githuber.scala
@@ -19,15 +19,49 @@ object Githuber extends App {
   implicit val defaultFormats: DefaultFormats.type        = DefaultFormats
 
   // TODO: поля можете добавить какие хотите
-  case class GithubUser()
-  case class GithubRepository()
+  case class GithubUser(login: String, name: String, location: Option[String], company: Option[String])
+  case class GithubRepository(name: String, description: Option[String],fork: Boolean, language: String)
 
   //  https://api.github.com/users/{$USER}
-  def getGithubUser(username: String): Future[GithubUser] = ???
+  def getGithubUser(username: String): Future[GithubUser] = Future {
+    parse(requests.get(s"https://api.github.com/users/$username").text)
+    .camelizeKeys.extract[GithubUser]
+  }
 
-  def getUserRepositories(repoUrl: String): Future[List[GithubRepository]] = ???
+  def getUserRepositories(username: String): Future[List[GithubRepository]] = Future {
+    parse(requests.get(s"https://api.github.com/users/$username/repos").text)
+      .camelizeKeys.extract[List[GithubRepository]]
+  }
 
-  def getUserInfo(username: String): Unit = ???
+  def getUserInfo(username: String): Unit = {
+    getGithubUser(username).onComplete {
+      case Success(gitInfo) =>
+        val name = gitInfo.name
+        val login = gitInfo.login
+        val location = gitInfo.location
+        val company = gitInfo.company
 
-  getUserInfo("")
+        println(s"Name: $name \nLogin: $login")
+        println(s"Location: ${location.getOrElse("no info")} " +
+          s"\nOrganization: ${company.getOrElse("no info")}")
+      case Failure(ex) => println(ex.getMessage)
+    }
+    Thread.sleep(3000) // Здесь не очень получилось(
+
+    getUserRepositories(username).onComplete {
+      case Success(repositories) => repositories.foreach(repo => {
+        val name = repo.name
+        val language = repo.language
+        val fork = repo.fork
+        val description = repo.description
+
+        println(s"Repository: $name, language: $language, ${if (fork) "forked" else "not forked"}")
+        println(s"\tDescription: ${description.getOrElse("no description")}")
+      })
+      case Failure(ex) => println(ex.getMessage)
+    }
+
+  }
+
+  getUserInfo("yelyaman")
 }
